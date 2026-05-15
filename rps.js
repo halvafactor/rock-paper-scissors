f38972b79c07b27b441ec0713713875f24b6e851
halvafactor <emily.laskin@gmail.com>
2026-05-14 20:42:56 -0700

    computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,16 @@
 console.log("hello, world");
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return "rock";
+    }
+    else if (choice === 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
